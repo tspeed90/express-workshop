475b23f8d4f9fb292ef50880d4aa0dc82ad0966c
tspeed90 <tspeed90@gmail.com>
2018-02-13 19:46:01 +0000

    accepting blog posts, adding it to a JSON object and rendering it to the page

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var formidable = require('express-formidable');
+var fs = require('fs');
+
+app.use(formidable());
+app.use(express.static('public'));
+
+app.post('/create-post', function(req, res) {
+  var post = req.fields.blogpost;
+  var currentDate = Date.now();
+
+  fs.readFile(__dirname + '/data/posts.json',  function (err, file) {
+    if (!err) {
+      var parsedFile = JSON.parse(file); 
+      parsedFile[currentDate] = post;
+      var fileToWrite = JSON.stringify(parsedFile);
+    }
+    
+    fs.writeFile(__dirname + '/data/posts.json', fileToWrite, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }); 
+});
+
+app.get('/get-posts', function (req, res) {
+  var file = __dirname + '/data/posts.json';
+    res.sendFile(file, function (err) {
+      if (err) {
+        console.log(file);
+      }
+    });
+});
+
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000!');
+});
